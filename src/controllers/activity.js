fb0970a259d4168760931c39fb164a47efbebb79
KennethLloyd <verastiguelloyd@gmail.com>
2020-07-05 14:00:57 +0800

    Delete activity route

--- src/controllers/activity.js
+++ src/controllers/activity.js
@@ -13,19 +13,6 @@ const addActivity = async (req, res) => {
   }
 };
 
-const editActivity = async (req, res) => {
-  try {
-    const editedActivity = await Activity.findByIdAndUpdate(req.params.id, {
-      name: req.body.name,
-    });
-
-    res.send({ activity: editedActivity });
-  } catch (e) {
-    console.log(e);
-    res.status(500).send({ error: 'Internal Server Error' });
-  }
-};
-
 const getActivities = async (req, res) => {
   try {
     const activities = await Activity.find({});
@@ -37,8 +24,37 @@ const getActivities = async (req, res) => {
   }
 };
 
+const editActivity = async (req, res) => {
+  try {
+    const editedActivity = await Activity.findByIdAndUpdate(
+      req.params.id,
+      {
+        name: req.body.name,
+      },
+      { new: true },
+    );
+
+    res.send({ activity: editedActivity });
+  } catch (e) {
+    console.log(e);
+    res.status(500).send({ error: 'Internal Server Error' });
+  }
+};
+
+const deleteActivity = async (req, res) => {
+  try {
+    const deletedActivity = await Activity.findByIdAndDelete(req.params.id);
+
+    res.send({ activity: deletedActivity });
+  } catch (e) {
+    console.log(e);
+    res.status(500).send({ error: 'Internal Server Error' });
+  }
+};
+
 module.exports = {
   addActivity,
-  editActivity,
   getActivities,
+  editActivity,
+  deleteActivity,
 };
--- src/routers/activity.js
+++ src/routers/activity.js
@@ -5,7 +5,12 @@ const { activityController } = require('../controllers');
 const router = new express.Router();
 
 router.post('/activities', authenticate, activityController.addActivity);
-router.put('/activities/:id', authenticate, activityController.editActivity);
 router.get('/activities', authenticate, activityController.getActivities);
+router.put('/activities/:id', authenticate, activityController.editActivity);
+router.delete(
+  '/activities/:id',
+  authenticate,
+  activityController.deleteActivity,
+);
 
 module.exports = router;
